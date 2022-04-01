8ff6fdbb931d5d060d2b0e41d95205252d8ab660
Jean Guillaume <jgi@jgwill.com>
2022-04-01 10:11:04 -0400

    add:img

--- docs/_acrylics_generator.sh
+++ docs/_acrylics_generator.sh
@@ -1,1 +1,1 @@
-(for f in $(du -a img/acrylics-painting/$1* | awk '/jpg/ {print $2}'); do echo '![]('$f')' ; done) > acrylics.md
+(for f in $(du -a img/acrylics-painting/ | awk '/jpg/ {print $2}'); do echo '![]('$f')' ; done) > acrylics.md
