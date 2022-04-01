c60e69773a26741d18194a7b5cdd60da87dea5a5
Jean Guillaume <jgi@jgwill.com>
2022-04-01 09:22:17 -0400

    fix:mn

--- docs/img/acrylics-painting/_mn_resizer.sh
+++ docs/img/acrylics-painting/_mn_resizer.sh
@@ -1,7 +1,11 @@
 # Normal
-mkdir -p mn;for f in *jpg; do convert $f -resize
- 500x -quality 100 mn/$f; done
+tdir=mn;mkdir -p $tdir
+for f in *jpg; do 
+	convert "$f" -resize 500x -quality 100 "$tdir/$f"
+done
 
 # Center cropped
-tdir=mncc;mkdir -p $tdir;for f in *jpg; do conve
-rt $f -resize 500x500\! -quality 100 $tdir/$f; done
+tdir=mncc;mkdir -p $tdir
+for f in *jpg; do 
+	convert "$f" -resize 500x500\! -quality 100 "$tdir/$f"
+done
