b87961b548600ac2c91c78cfca1014966e541932
JGWill Gaia.WSL <jgi@jgwill.com>
2022-04-08 05:06:44 -0400

    rebuild

--- docs/build.sh
+++ docs/build.sh
@@ -1,9 +1,18 @@
 
 
 # Rebuild the index/thumnail and ref acrylics.md page and publish it
-(wdir=img/acrylics-painting;cdir=$(pwd);cd $wdir && \
-	. _mn_resizer.sh && \
-	cd $cdir && \
-	. _acrylics_generator.sh && echo "Rebuilted" && \
-	git add $wdir/mn/*jpg $wdir/mncc/*jpg $wdir/*jpg &> /dev/null ;git commit acrylics.md $wdir/mn/*jpg $wdir/mncc/*jpg $wdir/*jpg -m rebuilt ;git push)
 
+## Acrylics
+export cdir=$(pwd)
+buildmn() {
+	local bdir="$1"
+(wdir=img/$bdir;cdir=$(pwd);cd $wdir && \
+	. $cdir/_mn_resizer.sh && \
+	cd $cdir && \
+	. $cdir/_mk_ref_sheet.sh && echo "Rebuilted" && \
+	git add $wdir/mn/*jpg $wdir/mncc/*jpg $wdir/*jpg &> /dev/null ;git commit $bdir.md $wdir/mn/*jpg $wdir/mncc/*jpg $wdir/*jpg -m rebuilt:$bdir ;git push)
+}
+
+# export bdir=acrylics-painting
+buildmn acrylics-painting
+buildmn ai-painting-collab
