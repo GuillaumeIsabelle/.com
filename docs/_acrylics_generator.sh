c451e888ddd871946b85dd6aad09de176b4062db
Jean Guillaume <jgi@jgwill.com>
2022-04-10 19:46:43 -0400

    tn

--- docs/_acrylics_generator.sh
+++
@@ -1,3 +0,0 @@
-header='|  file  |  img  |'
-sep='| --- | --- |'
-(echo "$header";echo "$sep";for f in $(du -a img/acrylics-painting/ | awk '/jpg/ {print $2}'); do ff=${f%.*};fff=$(echo $ff | sed -e 's/img\/acrylics-painting\///');echo '|['$fff']('$f')|!['$fff']('$f')|' ; done) > acrylics.md
--- docs/build.sh
+++ docs/build.sh
@@ -3,16 +3,10 @@
 # Rebuild the index/thumnail and ref acrylics.md page and publish it
 
 ## Acrylics
-export cdir=$(pwd)
-buildmn() {
-	local bdir="$1"
-(wdir=img/$bdir;cdir=$(pwd);cd $wdir && \
-	. $cdir/_mn_resizer.sh && \
-	cd $cdir && \
-	. $cdir/_mk_ref_sheet.sh && echo "Rebuilted" && \
-	git add $wdir/mn/*jpg $wdir/mncc/*jpg $wdir/*jpg &> /dev/null ;git commit $bdir.md $wdir/mn/*jpg $wdir/mncc/*jpg $wdir/*jpg -m rebuilt:$bdir ;git push)
-}
+. $binroot/__fn.sh
 
 # export bdir=acrylics-painting
-buildmn acrylics-painting
-buildmn ai-painting-collab
+mkimgmdmm acrylics-painting img yes
+mkimgmdmm ai-painting-collab img yes
+#buildmn acrylics-painting
+#buildmn ai-painting-collab
